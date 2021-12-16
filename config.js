c4daf2d3821b6ff9d4642a6c63150ab1046cc1fa
Dhvit <77879772+dhvitOP@users.noreply.github.com>
2021-12-16 23:25:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   mongourl : "", // MongoDB Url 
   callback: "", // The Callback for discord auth e.g - https://yourwebsite.com/callback
   botid: "", // Your Official botID
-  botsecret: "" // Your Official Bot Client Secret
+  botsecret: "", // Your Official Bot Client Secret,
+  bottoken: "" // Your Official Bot Token
 }
